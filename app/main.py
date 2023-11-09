bd828b84deaac526b692e3166c51cb3a3a034185
Josh Crowley <jcrowley317@gmail.com>
2023-11-08 17:08:43 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,44 @@
 import uvicorn
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-# import greenstalk
+import greenstalk
 import json
 import threading
 import time
+
+
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     print("Starting Survey Consumer...")
+#     threading.Thread(target=worker, daemon=True).start()
+#     yield
+#     print("Consumer stopped!")
+
+
+# # async def worker():
+# def worker():
+#     print("Starting Survey Consumer...")
+#     conn = greenstalk.Client(('127.0.0.1', 11300), use="response_tube", watch="survey_tube")
+#     while True:
+#             print("Waiting for job...")
+#             job = conn.reserve()
+#             print("Got job!")
+#             time.sleep(2)
+#             job_data = json.loads(job.body)
+#             print(job_data)
+#             print("Deleting job...")
+#             conn.delete(job)
+#             print("Job deleted!")
+
+# app = FastAPI(lifespan=lifespan)
+
+
+# ASYNC VERSION ##
 import asyncio
-import aiostalk
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
+from async_stalk import Client
+async def async_lifespan(app: FastAPI):
     print("Starting Survey Consumer...")
-    task = asyncio.create_task(worker())
+    task = asyncio.create_task(async_worker())
     yield
     task.cancel()
     try:
@@ -20,31 +46,30 @@ async def lifespan(app: FastAPI):
     except asyncio.CancelledError:
         pass
 
+async def async_worker():
+    async with Client(('127.0.0.1', 11300), use="response_tube", watch="survey_tube") as conn:
+        # print(f"Stats: {await conn.stats()}")
+        print(f"Tube States: {await conn.stats_tube(tube='survey_tube')}")
+        while True:
+            print("Waiting for job...")
+            job = await conn.reserve()
+            print("Got job!")
+            time.sleep(1)
+            job_data = json.loads(job.body)
+            print(job_data)
+            print("Deleting job...")
+            await conn.delete(job)
+            print("Job deleted!")
+            print("-"*20)
 
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=async_lifespan)
+
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello, world!"}
 
-
-async def worker():
-    print("Starting Survey Consumer...")
-    # conn = greenstalk.Client(('127.0.0.1', 11300), use="response_tube", watch="survey_tube")
-    async with aiostalk.Client(('127.0.0.1', 11300), use="response_tube", watch="survey_tube") as conn:
-        while True:
-            print(f"Stats: {await conn.stats()}")
-            print("Waiting for job...")
-            job = await conn.reserve()
-            print("Got job!")
-            time.sleep(2)
-            job_data = json.loads(job.body)
-            print(job_data)
-            print("Deleting job...")
-            await conn.delete(job)
-            print("Job deleted!")
-    print("Consumer stopped!")
 
 
 @app.get("/")
@@ -55,7 +80,7 @@ async def root():
 # 'action' endpoint and function
 @app.get("/health")
 async def health():
-    response = health_action()
+    response = await health_action()
     return response
 
 
@@ -64,5 +89,7 @@ async def health_action():
 
 
 if __name__ == "__main__":
+
+
     # should be running at port 8000
-    uvicorn.run("main:app", workers=1, reload=True)
+    uvicorn.run("main:app", workers=2, reload=True)
